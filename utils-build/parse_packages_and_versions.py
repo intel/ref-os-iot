83b87df6d6ffd1747c72dca8c0d673a5f12b195c
Tuomas Katila <tuomas.katila@intel.com>
2017-04-06 13:07:21 +0300

    utils-build: fix parse_packages_and_versions.py
    Signed-off-by: Tuomas Katila <tuomas.katila@intel.com>

--- utils-build/parse_packages_and_versions.py
+++ utils-build/parse_packages_and_versions.py
@@ -4,8 +4,8 @@ import sys
 
 if len(sys.argv) <= 1:
     print("%s <pn-depends.dot>" % sys.argv[0])
-    print("parses pn-depends.dot file and prints out all packages and their versions")
-    print("pn-depends.dot can be generated with bitbake: bitbake -g <image name>")
+    print("parses recipes-depends.dot file and prints out all packages and their versions")
+    print("recipes-depends.dot can be generated with bitbake: bitbake -g <image name>")
     sys.exit(1)
 
 packs = {}
@@ -13,11 +13,11 @@ packs = {}
 f = open(sys.argv[1])
 
 for line in f.readlines():
-    if line.find(" -> ") > 0:
-        break
-    if line.find("{") > 0:
+    if line.find("{") > 0 or line.find("}") > 0:
         continue
     if not line.startswith('"'):
+        continue
+    if line.find("[") < 0:
         continue
 
     p = line.split('"')[1]
@@ -29,3 +29,11 @@ f.close()
 
 for p in sorted(packs):
     print(p + " = " + packs[p])
+
+
+"""
+digraph depends {
+    "libunistring-native" [label="libunistring-native\n:0.9.7-r0\nvirtual:native:/media/bcache/mig_ise-mig_os2/build/../intel-iot-refkit/openembedded-core/meta/recipes-support/libunistring/libunistring_0.9.7.bb"]
+    "libunistring-native" -> "texinfo-dummy-native"
+    "libunistring-native" -> "gnu-config-native"
+"""
